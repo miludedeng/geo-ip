7f132eab6b01de920ba547d0564837b6a39fad79
miludedeng <hw@zving.com>
2020-05-01 17:18:18 +0800

    格式化代码

--- src/main/java/cc/cafetime/geoip/GeoIPService.java
+++ src/main/java/cc/cafetime/geoip/GeoIPService.java
@@ -14,27 +14,27 @@ import com.maxmind.geoip2.record.Country;
 import com.maxmind.geoip2.record.Subdivision;
 
 public class GeoIPService {
-	
-	 private DatabaseReader reader = null;
-	
+
+	private DatabaseReader reader = null;
 
 	public GeoIPService() throws IOException {
-		//GeoIP2-City 数据库文件D
-        File database = new File("data/GeoLite2-City.mmdb");
-        // 创建 DatabaseReader对象
-        reader = new DatabaseReader.Builder(database).build();
+		// GeoIP2-City 数据库文件D
+		File database = new File("data/GeoLite2-City.mmdb");
+		// 创建 DatabaseReader对象
+		reader = new DatabaseReader.Builder(database).build();
 	}
-	
+
 	public List<String> getLocation(String ip) throws IOException, GeoIp2Exception {
-        InetAddress ipAddress = InetAddress.getByName(ip);
-        // 获取查询结果
-        CityResponse response = reader.city(ipAddress);
-        // 获取国家信息
-        Country country = response.getCountry();
-        // 获取省份
-        Subdivision subdivision = response.getMostSpecificSubdivision();
-        //城市
-        City city = response.getCity();
-		return Arrays.asList(country.getNames().get("zh-CN"),subdivision.getNames().get("zh-CN") ,city.getNames().get("zh-CN"));
+		InetAddress ipAddress = InetAddress.getByName(ip);
+		// 获取查询结果
+		CityResponse response = reader.city(ipAddress);
+		// 获取国家信息
+		Country country = response.getCountry();
+		// 获取省份
+		Subdivision subdivision = response.getMostSpecificSubdivision();
+		// 城市
+		City city = response.getCity();
+		return Arrays.asList(country.getNames().get("zh-CN"), subdivision.getNames().get("zh-CN"),
+				city.getNames().get("zh-CN"));
 	}
 }
